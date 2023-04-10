123f1e4869c6bf7d53c4c26318b1d0d37ebf5fd2
Alex <opetrovskyi423@gmail.com>
2023-04-10 13:44:06 +0200

    portfolio_start

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/A1eeex'
+      className='link footer__link'
+    >
+      GIT Oleksandr Petrovskyi
+    </a>
+  </footer>
+)
+
+export default Footer
---
+++ src/portfolio.js
@@ -0,0 +1,68 @@
+const header = {
+  homepage: 'https://A1eeex.github.io/Portfolio',
+  title: 'JS.',
+}
+
+const about = {
+  // all the properties are optional - can be left empty or deleted
+  name: 'Oleksandr Petrovskyi',
+  role: ' Junior/Trainee Frontend developer',
+  description:
+    `Extremely passionate about software architecture and design combined
+    with sensitivity to serve the needs of the business balanced with the
+    delivery of high-quality solutions.
+    Having a positive outlook and am always willing to learn new traits and
+    expand my skills. Able to stretch the personal and work boundaries to
+    help your company stand out.`,
+  resume: 'https://drive.google.com/file/d/1z_bZEm92-6cPT2CTtmIGwgEKGbkN1-OI/view?usp=sharing',
+  social: {
+    linkedin: 'https://www.linkedin.com/in/oleksandr-petrovskyi-6b1010196/',
+    github: 'https://github.com/A1eeex',
+  },
+}
+
+const projects = [
+  {
+    name: 'Project 1',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Project 2',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Project 3',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+]
+
+const skills = [
+
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'Redux',
+  'SASS',
+  'Material UI',
+  'Git',
+]
+
+const contact = {
+  email: 'opetrovskyi423@gmail.com',
+}
+
+export { header, about, projects, skills, contact }
