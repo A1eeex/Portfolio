15e917e70737dcea923dc7b302d9f7a0cb163d01
Alex <opetrovskyi423@gmail.com>
2024-03-01 18:08:32 +0100

    update resume

--- src/portfolio.js
+++ src/portfolio.js
@@ -14,7 +14,7 @@ const about = {
     expand my skills. Able to stretch the personal and work boundaries to
     help your company stand out.`,
   resume:
-    'https://drive.google.com/file/d/1GUZKH2F84Z1sTmtD3lodDr4THVKCpIax/view',
+    'https://drive.google.com/file/d/1y2gQNCL0X3fNehVugLqSV7QCJoeUsjfa/view',
   social: {
     linkedin: 'https://www.linkedin.com/in/oleksandr-petrovskyi-6b1010196/',
     github: 'https://github.com/A1eeex',
